124a7b3bfd3736e69d80791db6b8d765092dd4ee
Kristine Dugan <kndugan@gmail.com>
2022-06-24 09:32:08 -1000

    Dropdown Descriptions Added

--- navbar.js
+++ navbar.js
@@ -4,30 +4,57 @@ function NavBar(){
    
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark mynav">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">BadBank</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">BadBank</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
             <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#/CreateAccount/">CreateAccount</a>
+
+            <li className="nav-item dropdown">
+                <a className="nav-link" href="#/CreateAccount/" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">CreateAccount</a>
+
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a className="dropdown-item">Create an Account</a></li>
+                </ul>
               </li>
-              <li className="nav-item">
+
+              {/* <li className="nav-item">
                 <a className="nav-link" aria-current="page" href="#/login/">Login</a>
+              </li> */}
+
+              <li className="nav-item dropdown">
+                <a className="nav-link" href="#/deposit/" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Deposit</a>
+
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a className="dropdown-item">Make a Deposit</a></li>
+                </ul>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#/deposit/">Deposit</a>
+              
+              <li className="nav-item dropdown">
+                <a className="nav-link" href="#/withdraw/" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Withdraw</a>
+
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a className="dropdown-item">Withdraw Funds</a></li>
+                </ul>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#/withdraw/">Withdraw</a>
+
+              {/* <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Balance</a>
+
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a className="dropdown-item" href="#/balance/">View Your Balance</a></li>
+                </ul>
+              </li> */}
+              
+              <li className="nav-item dropdown">
+                <a className="nav-link" href="#/alldata" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">AllData</a>
+
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a className="dropdown-item">View Data</a></li>
+                </ul>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#/balance/">Balance</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#/alldata/">AllData</a>
-              </li>
+
             </ul>
           </div>
         </div>
